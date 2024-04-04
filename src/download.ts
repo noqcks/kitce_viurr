2d5e66883e11a383a5211791c16341cfd3297052
noqcks <benji@093b.org>
2024-04-04 17:07:07 +0000

    Upgrade dependencies

--- src/download.ts
+++ src/download.ts
@@ -70,7 +70,7 @@ export const cover = async (productId: string): Promise<[ISeries, IEpisode, stri
   }
   debug('cover image file:', filepath);
   const directory = path.dirname(filepath);
-  await mkdirp(directory);
+  await mkdirp(directory, { recursive: true });
   await fs.writeFile(filepath, buffer);
   return [series, episode, filepath];
 };
